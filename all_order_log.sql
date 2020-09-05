08fdc7127a766e51f55423801ff559d6ac5ece9a
wangtianyu61 <wangtianyu6162@gmail.com>
2020-09-05 14:41:31 +0800

    change the sql files

--- all_order_log.sql
+++ all_order_log.sql
@@ -1,4 +1,4 @@
-INSERT INTO all_order_log VALUES (1, 0, 2, 1001, -1);
-INSERT INTO all_order_log VALUES (2, 1, -1, -1, 4001);
-INSERT INTO all_order_log VALUES (3, 0, 3, 1002, -1);
+INSERT INTO `all_order_log` (order_id, order_type, table_id, serial, takeout) VALUES (1, 0, 2, 1001, -1);
+INSERT INTO `all_order_log` (order_id, order_type, table_id, serial, takeout) VALUES (2, 1, -1, -1, 4001);
+INSERT INTO `all_order_log` (order_id, order_type, table_id, serial, takeout) VALUES (3, 0, 3, 1002, -1);
 
