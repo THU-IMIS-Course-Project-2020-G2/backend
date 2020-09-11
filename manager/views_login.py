488133e17331ca8a48b55856e8c52c7bd157efcb
wangtianyu61 <wangtianyu6162@gmail.com>
2020-09-11 12:43:59 +0800

    change the login info

--- manager/views_login.py
+++ manager/views_login.py
@@ -10,8 +10,8 @@ from django.db.models import Sum, Count, Max, Min, Avg
 
 def login_check(request):
     dict_data = json.loads(request.body, strict = False)
-    mg_info = managerInfo.objects.filter(user_id = dict_data['user_id'], password = dict_data['password'], type = dict_data['type'])
-    login_response = {"user_id":dict_data['user_id'], "success":0, "power":dict_data['type']}
+    mg_info = managerInfo.objects.filter(userid = dict_data['userid'], password = dict_data['password'], user_type = dict_data['user_type'])
+    login_response = {"userid":dict_data['userid'], "success":0, "power":dict_data['user_type']}
     # 登录失败
     if len(mg_info) == 0:
         login_response['success'] = 1
